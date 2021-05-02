475bf38a2baf5bb4ec0149bc7d020420412326cd
parianspartez <hossein.amiriparian@spartez-software.com>
2021-05-02 20:34:28 +0200

    connect to postgres

--- src/main/java/ir/parian/loan/data/PersistenceConfig.java
+++ src/main/java/ir/parian/loan/data/PersistenceConfig.java
@@ -1,5 +1,8 @@
 package ir.parian.loan.data;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -7,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
 import java.security.Principal;
 import java.util.Optional;
 
@@ -14,6 +18,17 @@ import java.util.Optional;
 @EnableTransactionManagement
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class PersistenceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+
     @Bean
     public AuditorAware<String> auditorAware() {
         return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication()).map(Principal::getName);
